dd8d1f6e47853fc9f0ef95c4bb531fa734fa3160
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-09-01 23:27:49 +0900

    first commit

---
+++ resources/views/livewire/counter-sample.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div style="text-align: center">
+        <button type="button" class="btn btn-primary"
+            style="--bs-btn-padding-y: .80rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+            wire:click="increment">
+            Count Up
+        </button>
+        <h1>{{ $count }}</h1>
+    </div>
+</div>
